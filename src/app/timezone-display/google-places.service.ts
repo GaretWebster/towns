1de01ff78ca321a0869690bbdb9ec5694a2bc57c
Garet Webster <gdubrocks@gmail.com>
2018-10-15 00:04:01 -0700

    update on service

---
+++ src/app/timezone-display/google-places.service.ts
@@ -0,0 +1,100 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+export interface Cities {
+  geonameid: number;
+  name: string;
+  asciiname: string;
+  alternatenames: string;
+  latitude: number;
+  longitude: number;
+  feature_class: string;
+  feature_code: string;
+  country_code: string;
+  cc2: string;
+  admin1_code: string;
+  admin2_code: string;
+  admin3_code: string;
+  admin4_code: string;
+  population: number;
+  elevation: number;
+  dem: number;
+  timezone: string;
+  modification_date: string;
+}
+
+export interface Config {
+  heroesUrl: string;
+  textfile: string;
+}
+
+//@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
+export class GooglePlacesService {
+  citiesURL = 'assets/cities.json';
+  configUrl = 'assets/googlePlacesConfig.json'
+
+  constructor(private http: HttpClient) { }
+
+  getCities() {
+    return this.http.get<Cities>(this.citiesURL);
+  }
+  
+  getConfig() {
+    return this.http.get<Config>(this.configUrl)
+      .pipe(
+        retry(3), // retry a failed request up to 3 times
+        catchError(this.handleError) // then handle the error
+      );
+  }
+
+  getConfig_1() {
+    return this.http.get(this.configUrl);
+  }
+
+  getConfig_2() {
+    // now returns an Observable of Config
+    return this.http.get<Config>(this.configUrl);
+  }
+
+  getConfig_3() {
+    return this.http.get<Config>(this.configUrl)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  getConfigResponse(): Observable<HttpResponse<Config>> {
+    return this.http.get<Config>(
+      this.configUrl, { observe: 'response' });
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
+
+  makeIntentionalError() {
+    return this.http.get('not/a/real/url')
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+}
--- src/app/timezone-display/timezone-display.component.ts
+++ src/app/timezone-display/timezone-display.component.ts
@@ -1,28 +1,113 @@
 import { Component, OnInit } from '@angular/core';
-import * as options from 'cities.json';
+
 import { FormControl } from '@angular/forms';
-import {Observable} from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
-	
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { GooglePlacesService, Config, Cities } from './google-places.service'
+
+//import * as options from 'cities.json';
+
+/*export interface Cities {
+  geonameid: number;
+  name: string;
+  asciiname: string;
+  alternatenames: string;
+  latitude: number;
+  longitude: number;
+  feature_class: string;
+  feature_code: string;
+  country_code: string;
+  cc2: string;
+  admin1_code: string;
+  admin2_code: string;
+  admin3_code: string;
+  admin4_code: string;
+  population: number;
+  elevation: number;
+  dem: number;
+  timezone: string;
+  modification_date: string;
+}*/
+
 @Component({
   selector: 'app-timezone-display',
   templateUrl: './timezone-display.component.html',
+  providers: [GooglePlacesService],
   styleUrls: ['./timezone-display.component.css']
 })
-export class TimezoneDisplayComponent implements OnInit {  
-  //word : string = option5[0].name;
+export class TimezoneDisplayComponent implements OnInit {
+  error: any;
+  headers: string[];
+  city: Cities;
+  config: Config;
 
-  stateControl = new FormControl();
-  //options: string[] = ['One', 'Two', 'Three'];
-  filteredOptions: Observable<string[]>;
+  constructor(private googlePlacesService: GooglePlacesService) { }
+
+  clear() {
+    this.config = undefined;
+    this.error = undefined;
+    this.headers = undefined;
+  }
 
   ngOnInit() {
+    //this.getCities();
+
     this.filteredOptions = this.stateControl.valueChanges
       .pipe(
         startWith(''),
         map(value => this._filter(value))
       );
   }
+
+  showConfig() {
+    this.googlePlacesService.getConfig()
+      .subscribe(
+        (data: Config) => this.config = { ...data }, // success path
+        error => this.error = error // error path
+      );
+  }
+
+  showConfig_v1() {
+    this.googlePlacesService.getConfig_1()
+      .subscribe((data: Config) => this.config = {
+        heroesUrl: data['heroesUrl'],
+        textfile: data['textfile']
+      });
+  }
+
+  showConfig_v2() {
+    this.googlePlacesService.getConfig()
+      // clone the data object, using its known Config shape
+      .subscribe((data: Config) => this.config = { ...data });
+  }
+
+  showConfigResponse() {
+    this.googlePlacesService.getConfigResponse()
+      // resp is of type `HttpResponse<Config>`
+      .subscribe(resp => {
+        // display its headers
+        const keys = resp.headers.keys();
+        this.headers = keys.map(key =>
+          `${key}: ${resp.headers.get(key)}`);
+
+        // access the body directly, which is typed as `Config`.
+        this.config = { ...resp.body };
+      });
+  }
+
+  makeError() {
+    this.googlePlacesService.makeIntentionalError().subscribe(null, error => this.error = error);
+  }
+
+  /*showCities() {
+    this.googlePlacesService.getCities()
+      .subscribe((data: Cities) => this.city = { ...data });
+  }
+  options = this.city.name;*/
+
+  stateControl = new FormControl();
+  //options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
 
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
