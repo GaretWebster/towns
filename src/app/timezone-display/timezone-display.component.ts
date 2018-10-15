e477662ca3fccf4460f2e424cea2558bf4d944fc
Garet Webster <gdubrocks@gmail.com>
2018-10-15 00:51:22 -0700

    Adding more cities

--- src/app/timezone-display/timezone-display.component.ts
+++ src/app/timezone-display/timezone-display.component.ts
@@ -7,28 +7,6 @@ import { GooglePlacesService, Config, Cities } from './google-places.service'
 
 //import * as options from 'cities.json';
 
-/*export interface Cities {
-  geonameid: number;
-  name: string;
-  asciiname: string;
-  alternatenames: string;
-  latitude: number;
-  longitude: number;
-  feature_class: string;
-  feature_code: string;
-  country_code: string;
-  cc2: string;
-  admin1_code: string;
-  admin2_code: string;
-  admin3_code: string;
-  admin4_code: string;
-  population: number;
-  elevation: number;
-  dem: number;
-  timezone: string;
-  modification_date: string;
-}*/
-
 @Component({
   selector: 'app-timezone-display',
   templateUrl: './timezone-display.component.html',
@@ -50,8 +28,6 @@ export class TimezoneDisplayComponent implements OnInit {
   }
 
   ngOnInit() {
-    //this.getCities();
-
     this.filteredOptions = this.stateControl.valueChanges
       .pipe(
         startWith(''),
@@ -65,20 +41,6 @@ export class TimezoneDisplayComponent implements OnInit {
         (data: Config) => this.config = { ...data }, // success path
         error => this.error = error // error path
       );
-  }
-
-  showConfig_v1() {
-    this.googlePlacesService.getConfig_1()
-      .subscribe((data: Config) => this.config = {
-        heroesUrl: data['heroesUrl'],
-        textfile: data['textfile']
-      });
-  }
-
-  showConfig_v2() {
-    this.googlePlacesService.getConfig()
-      // clone the data object, using its known Config shape
-      .subscribe((data: Config) => this.config = { ...data });
   }
 
   showConfigResponse() {
@@ -99,11 +61,14 @@ export class TimezoneDisplayComponent implements OnInit {
     this.googlePlacesService.makeIntentionalError().subscribe(null, error => this.error = error);
   }
 
-  /*showCities() {
+  showCities() {
     this.googlePlacesService.getCities()
-      .subscribe((data: Cities) => this.city = { ...data });
+      .subscribe(
+        (data: Cities) => this.city = { ...data }, // success path
+        error => this.error = error // error path
+      );
   }
-  options = this.city.name;*/
+  //options = this.city.name;
 
   stateControl = new FormControl();
   //options: string[] = ['One', 'Two', 'Three'];
